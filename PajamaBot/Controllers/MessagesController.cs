0af8caa5373baa6cff49411e303f87c14790df06
Gabilin <ava.alexey@gmail.com>
2016-04-30 10:06:44 +0300

    Alarm simple implementation

--- PajamaBot/Controllers/MessagesController.cs
+++ PajamaBot/Controllers/MessagesController.cs
@@ -8,6 +8,8 @@ using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Utilities;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using PajamaBot.Controllers;
 
 namespace PajamaBot
 {
@@ -22,63 +24,76 @@ namespace PajamaBot
         {
             if (message.Type == "Message")
             {
-
-                string reply;
-                var counter = message.GetBotPerUserInConversationData<int>("counter");
-                if (counter == 0 )
-                {
-                    reply = "Hey there! How Can I help you?\\n1) Create a project\\n2)Change Project Settings\\n3)Show project info & stats";
-                    counter = 1;
-                }
-                else if (counter == 1)
-                {
-                    if (message.Text.StartsWith("1") || message.Text.StartsWith("first"))
-                    {
-                        reply = "Okey, let us create a new project.In you next reply, specify the TFS iteration, project team and email i can use to notify the whole team.\\Example:" + @"Products\SC8.1\Update-3\Sustained Engineering\SC8.1U3-Pool for bugfixing" + "|" + "AVA;SMAR;DS" + "|" + "DepartmentPRodcutSEteam@sitecore.net";
-                        counter = 2;
-                    }
-                    else
-                    {
-                        reply = "Not implemented";
-                        counter = 0;
-                    }
-                }
-                else if (counter == 2)
-                {
-                    //Here goes the parsing of the input parameters and confirmation
-                    reply = "Not Implemnted";
-                    counter = 0;
-                }
-                else
-                {
-                    reply = "I am not so smart, sorry!";
-                    counter = 0;
-                }
-
-                var replyMessage = message.CreateReplyMessage(reply);
-
-                replyMessage.SetBotPerUserInConversationData("counter", counter);
-
-                return replyMessage;
-                
-
-                //// fetch our state associated with a user in a conversation. If we don't have state, we get default(T)
-                //var counter = message.GetBotPerUserInConversationData<int>("counter");
-
-                //// create a reply message   
-                //Message replyMessage = message.CreateReplyMessage($"{++counter} You said:{message.Text}");
-
-                //// save our new counter by adding it to the outgoing message
-                //replyMessage.SetBotPerUserInConversationData("counter", counter);
-
-                //// return our reply to the user
-                //return replyMessage;
-
+                // return our reply to the user
+                return await Conversation.SendAsync(message, () => new SimpleAlarmDialog());
             }
             else
             {
                 return HandleSystemMessage(message);
             }
+        }
+
+        private void Test()
+        {
+            //if (message.Type == "Message")
+            //{
+
+            //    string reply;
+            //    var counter = message.GetBotPerUserInConversationData<int>("counter");
+            //    if (counter == 0)
+            //    {
+            //        reply = "Hey there! How Can I help you?\\n1) Create a project\\n2)Change Project Settings\\n3)Show project info & stats";
+            //        counter = 1;
+            //    }
+            //    else if (counter == 1)
+            //    {
+            //        if (message.Text.StartsWith("1") || message.Text.StartsWith("first"))
+            //        {
+            //            reply = "Okey, let us create a new project.In you next reply, specify the TFS iteration, project team and email i can use to notify the whole team.\\Example:" + @"Products\SC8.1\Update-3\Sustained Engineering\SC8.1U3-Pool for bugfixing" + "|" + "AVA;SMAR;DS" + "|" + "DepartmentPRodcutSEteam@sitecore.net";
+            //            counter = 2;
+            //        }
+            //        else
+            //        {
+            //            reply = "Not implemented";
+            //            counter = 0;
+            //        }
+            //    }
+            //    else if (counter == 2)
+            //    {
+            //        //Here goes the parsing of the input parameters and confirmation
+            //        reply = "Not Implemnted";
+            //        counter = 0;
+            //    }
+            //    else
+            //    {
+            //        reply = "I am not so smart, sorry!";
+            //        counter = 0;
+            //    }
+
+            //    var replyMessage = message.CreateReplyMessage(reply);
+
+            //    replyMessage.SetBotPerUserInConversationData("counter", counter);
+
+            //    return replyMessage;
+
+
+            //    //// fetch our state associated with a user in a conversation. If we don't have state, we get default(T)
+            //    //var counter = message.GetBotPerUserInConversationData<int>("counter");
+
+            //    //// create a reply message   
+            //    //Message replyMessage = message.CreateReplyMessage($"{++counter} You said:{message.Text}");
+
+            //    //// save our new counter by adding it to the outgoing message
+            //    //replyMessage.SetBotPerUserInConversationData("counter", counter);
+
+            //    //// return our reply to the user
+            //    //return replyMessage;
+
+            //}
+            //else
+            //{
+            //    return HandleSystemMessage(message);
+            //}
         }
 
         private Message HandleSystemMessage(Message message)
